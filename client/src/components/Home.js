bd2ddf5df1e84f24aa660ffdf7c6ce608692cf83
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-06 15:22:48 -0600

    make copy of App.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,52 @@
+import './App.css';
+import React, { useState } from 'react';
+import Button from './components/Button.js';
+
+const App = () => {
+
+  const [input, setInput] = useState('');
+
+  const clearForm = () => {
+    setInput('');
+  }
+  
+  const handleInput = (e) => {
+    setInput(e.target.value);
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    let post = {
+      userName: "",
+      confession: input,
+      time: "test time"
+    }
+
+    await fetch("http://localhost:5000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    })
+    .catch(err => {
+      console.error(err);
+      return;
+    });
+    clearForm();
+  }
+
+  return (
+    <div className='App'>
+      <label htmlFor='confession'>confess</label>
+      <form name='confession' className='confession'>
+        <textarea value={input} onChange={handleInput}></textarea>
+        <Button onClick={onSubmit} label='Submit'/>
+      </form>
+
+    </div>
+  );
+}
+
+export default App;
