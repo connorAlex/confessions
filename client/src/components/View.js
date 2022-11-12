d391b338c732ab555e9fa09a2d246a8fcab4f9bf
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-12 12:25:15 -0600

    add skeleton for loading state

--- client/src/components/View.js
+++ client/src/components/View.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate, useLocation} from "react-router-dom";
 import Navbar from "./Navbar";
+import Skeleton, {SkeletonTheme} from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import './styles/View.css'
 
 const View = React.memo(() => {
 
     const [confession, setConfession ] = useState();
+    const [isFading, setIsFading] = useState();
     const navigate = useNavigate();
     const {state} = useLocation();
 
@@ -15,6 +18,16 @@ const View = React.memo(() => {
         };
         getConfession();
     }, []);
+
+    // useEffect(() => {
+            
+    //     if (confession) {
+    //         setIsFading('fade-out');
+            
+    //     }
+    
+    //     return () => clearInterval(timeout);
+    // }, [isFading])
 
     let getConfession = async () => {
         let response = await fetch("http://localhost:5000/view");
@@ -33,9 +46,11 @@ const View = React.memo(() => {
     return (
         <div className="View">
             <div className = "message">
-                <div>Another's confession.</div>
-                <div>{confession}</div>
-                <div>-Anon</div>
+                <SkeletonTheme  baseColor="#f3f4f6" highlightColor="#d1d5db">
+                    <div>Another's confession.</div>
+                    <div className="confessionText">{!confession? (<Skeleton count={4}/>) : (<div style={{animation: `fadeIn 1s`}}> {confession}</div>) }</div>
+                    <div className="signature">{!confession? (<Skeleton className={`signature`} count={1}/>) : (<div style={{animation: `fadeIn 1s`}}>-Anon</div>) }</div>
+                </SkeletonTheme>
             </div>
             <Navbar />
         </div>
