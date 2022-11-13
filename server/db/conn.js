5cbd5a7070e27a3b81499e98d8a641d1ba3e0993
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-13 07:47:05 -0600

    server prettier formatting

--- server/db/conn.js
+++ server/db/conn.js
@@ -5,18 +5,20 @@ let dbName = "test_db";
 let db_connect;
 
 module.exports = {
-    run: async () => {
-        try {
-            db_connect = await client.connect();
-            console.log("Connected Correctly to DB");
-            return db_connect;
-        } catch (err) {
-            console.log(err.stack);
-        }
-    },
-    close: async () => {
-        await client.close();
-        console.log("Close DB Connection")
-    },
-    getDb: async () => {return await client.db("test_db")}
+  run: async () => {
+    try {
+      db_connect = await client.connect();
+      console.log("Connected Correctly to DB");
+      return db_connect;
+    } catch (err) {
+      console.log(err.stack);
+    }
+  },
+  close: async () => {
+    await client.close();
+    console.log("Close DB Connection");
+  },
+  getDb: async () => {
+    return await client.db("test_db");
+  },
 };
--- server/routes/record.js
+++ server/routes/record.js
@@ -2,8 +2,7 @@ const { response } = require("express");
 const express = require("express");
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
-const profanity = require('@2toad/profanity').profanity;
-
+const profanity = require("@2toad/profanity").profanity;
 
 //router is an instance of the express router. Will be used to define routes.
 // https://expressjs.com/en/guide/routing.html
@@ -11,43 +10,49 @@ const router = express.Router();
 
 // get a random confession.
 router.get("/view", async (req, res) => {
-    const db_connection = await dbo.run();
-    const db = db_connection.db("test_db");
+  const db_connection = await dbo.run();
+  const db = db_connection.db("test_db");
 
-    try {
-        let randomConfession = await db.collection("test_collection").aggregate([{ $sample: {size: 1} }]).toArray().then(data => data);
-        res.send(await randomConfession[0]);
-    } catch (err) {
-        console.error(err.stack);
-    }
+  try {
+    let randomConfession = await db
+      .collection("test_collection")
+      .aggregate([{ $sample: { size: 1 } }])
+      .toArray()
+      .then((data) => data);
+    res.send(await randomConfession[0]);
+  } catch (err) {
+    console.error(err.stack);
+  }
 
-    dbo.close();
+  dbo.close();
 });
 
 // Create a new confession
-router.post("/", async (req,res) => {
-    const db_connection = await dbo.run();
-    const db = db_connection.db("test_db");
+router.post("/", async (req, res) => {
+  const db_connection = await dbo.run();
+  const db = db_connection.db("test_db");
 
-    // https://github.com/2Toad/Profanity Profanity Check - Thanks 2Toad!
-    if (profanity.exists(req.body.confession)) {
-        db_connection.close();
-        return;
-    };
-
-    const newConfession  = {
-        "userName": req.body.userName,
-        "confession": req.body.confession,
-        "time": req.body.time
-    }
-    try {
-        const postResult = await db.collection("test_collection").insertOne(newConfession);
-        console.log( await postResult);
-    } catch (err) {
-        console.error(err.stack);
-    }
+  // https://github.com/2Toad/Profanity Profanity Check - Thanks 2Toad!
+  if (profanity.exists(req.body.confession)) {
     db_connection.close();
-    res.json("record added");
+    return;
+  }
+
+  const newConfession = {
+    userName: req.body.userName,
+    confession: req.body.confession,
+    time: req.body.time,
+  };
+  try {
+    const postResult = await db
+      .collection("test_collection")
+      .insertOne(newConfession);
+    console.log(await postResult);
+  } catch (err) {
+    console.error(err.stack);
+  }
+  db_connection.close();
+  res.json("record added");
 });
 
 module.exports = router;
